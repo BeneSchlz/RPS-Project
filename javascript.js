d760b034ca6eb231c24c8898a8286e1ff69aaa13
BeneSchlz <123456789+odin@bschulz735@gmail.com>
2024-05-24 22:08:36 +0200

    Added Function HumanChoice and Computer

--- javascript.js
+++ javascript.js
@@ -1,5 +1,23 @@
-console.log("Hello, World!")
-
 function getComputerChoice () {
-    
+    let random = Math.floor(Math.random()*3)
+    if (random === 1) {
+        return "rock"
+    } else if (random === 2){
+        return "paper"
+    }
+    else {
+        return "scissors"
+    }
 }
+getComputerChoice ()
+console.log(getComputerChoice());
+
+function getHumanChoice () {
+    let humanChoice = prompt("Choose Rock, Paper, or Scissors");
+    humanChoice = humanChoice.toLowerCase();
+    while (humanChoice !== "rock" && humanChoice !== "paper" && humanChoice !== "scissors") {
+        humanChoice = prompt("Invalid choice. Please choose Rock, Paper, or Scissors").toLowerCase();
+    }
+    return humanChoice;
+}
+console.log(getHumanChoice());
